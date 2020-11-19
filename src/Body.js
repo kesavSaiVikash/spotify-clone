cd60ff88d35de9806e623312df2e948172492d19
vikash naidu <vikashnaidu@vikashs-MacBook-Air.local>
2020-11-19 00:11:02 -0500

    Initial import

---
+++ src/Body.js
@@ -0,0 +1,82 @@
+import React from "react";
+import "./Body.css";
+import Header from "./Header";
+import { useStateValue } from "./StateProvider";
+import SongRow from "./SongRow";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+
+function Body({ spotify }) {
+  const [{ discover_weekly }, dispatch] = useStateValue();
+
+  const playPlaylist = (id) => {
+    spotify
+      .play({
+        context_uri: `spotify:playlist:37i9dQZEVXcJZyENOWUFo7`,
+      })
+      .then((res) => {
+        spotify.getMyCurrentPlayingTrack().then((r) => {
+          dispatch({
+            type: "SET_ITEM",
+            item: r.item,
+          });
+          dispatch({
+            type: "SET_PLAYING",
+            playing: true,
+          });
+        });
+      });
+  };
+
+  const playSong = (id) => {
+    spotify
+      .play({
+        uris: [`spotify:track:${id}`],
+      })
+      .then((res) => {
+        spotify.getMyCurrentPlayingTrack().then((r) => {
+          dispatch({
+            type: "SET_ITEM",
+            item: r.item,
+          });
+          dispatch({
+            type: "SET_PLAYING",
+            playing: true,
+          });
+        });
+      });
+  };
+
+  return (
+    <div className="body">
+      <Header spotify={spotify} />
+
+      <div className="body__info">
+        <img src={`https://media-exp1.licdn.com/dms/image/C4D03AQE7nwK7USVgEw/profile-displayphoto-shrink_200_200/0?e=1611187200&v=beta&t=ubH9otXD_ljcti76mug3DuJ7yQOea5jfo-iiFVtnmDM`} alt="" />
+        <div className="body__infoText">
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>{discover_weekly?.description}</p>
+        </div>
+      </div>
+
+      <div className="body__songs">
+        <div className="body__icons">
+          <PlayCircleFilledIcon
+            className="body__shuffle"
+            onClick={playPlaylist}
+          />
+          <FavoriteIcon fontSize="large" />
+          <MoreHorizIcon />
+        </div>
+
+        {discover_weekly?.tracks.items.map((item) => (
+          <SongRow playSong={playSong} track={item.track} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Body;
